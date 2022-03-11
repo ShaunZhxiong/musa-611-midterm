3cf3a13d178c31d4cc95fc24232191343e6fae48
Shaun.xiongzheng <shaun_zheng@163.com>
2022-03-10 19:18:41 -0500

    frame work done

--- index.js
+++ index.js
@@ -1,153 +1,244 @@
-/* global schools */
+/* global map */
 
-const schoolMap = L.map('school-map').setView([39.95303901388685, -75.16341794003617], 13);
-const schoolLayer = L.layerGroup().addTo(schoolMap);
-const highlightLayer = L.layerGroup().addTo(schoolMap);
+const dogMap = L.map('dog-map',{scrollWheelZoom: false}).setView([40.72995787857809, -73.99271702327988], 13);
+const layerGroup = L.layerGroup().addTo(dogMap);
 
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  subdomains: 'abcd',
-  minZoom: 0,
-  maxZoom: 18,
-  ext: 'png',
-}).addTo(schoolMap);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+}).addTo(dogMap);
 
-const HighLightSchool = document.querySelector('#HighLight-School');
-const schoolList = document.querySelector('#school-list');
-const gradeLevelSelect = document.querySelector('#grade-level-select');
-const zipCodeSelect = document.querySelector('#zip-code-select');
+const dogLicensesBoundSmall = [
+	[40.70250,-74.02656],
+	[40.70250,-73.93524],
+	[40.79509,-73.93524],
+	[40.79509,-74.02656],
+	[40.70250,-74.02656]
+];
 
-const redIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
+const petStoresBound = [
+	[40.660847697284815,-74.0478515625],
+	[40.660847697284815,-73.87859344482422],
+	[40.823682398765996,-73.87859344482422],
+	[40.823682398765996,-74.0478515625],
+	[40.660847697284815,-74.0478515625]
+];
 
-/* ====================
+/* current slide index */
+let currentSlideIndex = 0;
 
-# Exercise: School Explorer (redux)
+const slideDivs = document.getElementsByClassName('slide');
 
-==================== */
+function calcCurrentSlideIndex() {
+  const scrollPos = window.scrollY + window.innerHeight;
+  let i;
+  for (i = 0; i < slideDivs.length; i++) {
+    const slidePos = slideDivs[i].offsetTop;
+    if (slidePos > scrollPos) {
+      break;
+    }
+  }
 
-let HighlightSchoolFunction = (childArray) => {
-  let HighLightSchoolInfo = [
-    `Asian American %: ${childArray.AsianPCT}`,
-    `Black African American %: ${childArray.BlackAfricanAmericanPCT}`,
-    `Hispantic American %: ${childArray.HispanicPCT}`,
-    `White American %: ${childArray.WhitePCT}`];
-  let el4 = htmlToElement(`<strong><li style='color:#CA263D;'>Grades: ${childArray.GradeLevel}</li></strong>`);
-  HighLightSchool.appendChild(htmlToElement('<li style="color:#CA263D;"> <br></li>')).appendChild(el4);
-  HighLightSchoolInfo.forEach(eachInfo => {
-    let el2 = htmlToElement(`<li style='color:#CA263D;'>${eachInfo}</li>`);
-    HighLightSchool.appendChild(el2);
-  });
-  document.querySelector('body').style.gridTemplateAreas = '"filters filters sorters nouse"  "map map list list2"';
+  if (i === 0) {
+    currentSlideIndex = 0;
+		layerGroup.clearLayers();
+  } else if (currentSlideIndex != i-1) {
+    currentSlideIndex = i - 1;
+		if (currentSlideIndex === 0) {
+			layerGroup.clearLayers();
+			} else if (currentSlideIndex === 1) {
+			updateDogRunsMarkers(dogRuns);
+			} else if (currentSlideIndex === 2) {
+			updatedogLicensesMarkers(dogLicenses);
+			updatedogLicensesSmallBounds();
+			} else if (currentSlideIndex === 3) {
+			updatedogLicensesMarkers(dogLicenses);
+			} else if (currentSlideIndex === 4) {
+			updatePetStoresMarkers(petStores);
+			};
+  }
+}
+/* show the dog breeds */
+
+const dogLicensesStyle = {
+  color: "#FFD000",
+  fillColor: "#FFD000",
+  fillOpacity: 0.5,
 };
 
-let showSchoolInfo = (school, marker) => {
-  const dataUrl = `../../data/demographics/${school['ULCS Code']}.json`;
-  fetch(dataUrl)
-    .then(resp => resp.json())
-    .then(data => {
-      // let allGrades = data[-0];
-      let allGrades = data
-        .filter(everyGrade => everyGrade.GradeLevel === 'All Grades')[0];
-      const sector = allGrades.Sector;
-      const subSector = allGrades.SubSector;
-      const grade = data
-        .filter(everyGrade => everyGrade.GradeLevel !== 'All Grades')
-        .map(everyGrade => everyGrade.GradeLevel);
-      const AVGpctm = allGrades.MalePCT;
-      const AVGpctf = allGrades.FemalePCT;
-
-      marker.bindPopup(`
-        <h6>${school['Publication Name']}</h6>
-        <ul>
-          <li>School Level: ${school['School Level']}</li>
-          <li>Admission Type: ${school['Admission Type']}</li>
-          <li>Sector: ${sector} - ${subSector}</li>
-          <li>Grades: ${grade}</li>
-          <li>Average Percent Male: ${AVGpctm}%</li>
-          <li>Average Percent Female: ${AVGpctf}%</li>
-        </ul>`).openPopup();
-
-      HighLightSchool.innerHTML = '';
-      data.filter(everyGrade => everyGrade.GradeLevel !== 'All Grades').map(childArray => HighlightSchoolFunction(childArray));
+let updatedogLicensesMarkers = (dogLicenses) => {
+  /* celar layer*/
+  layerGroup.clearLayers();
+  /* fly to bounds*/
+  let dogLicensesBound = L.geoJSON(dogLicenses,{style: dogRunsStyle});
+  dogMap.flyToBounds(dogLicensesBound.getBounds());
+  /* Loop each dog store to plot it*/
+  dogLicenses.forEach(dogLicense => {
+    let circleMarker = L.circle([dogLicense.geometry.coordinates[1], dogLicense.geometry.coordinates[0]],40, dogLicensesStyle)
+    .bindTooltip(dogLicense.properties.GEOID.toString())
+    .addTo(layerGroup);
+    let dogLicensesGEOID = dogLicense.properties.GEOID.toString();
+  /* Add event listener*/
+    circleMarker.addEventListener('click', () => {
+      circleMarker.bindPopup(
+        `<h6>${dogLicensesGEOID}</h6>`
+        ).openPopup();
     });
+  });
 };
 
-/* PASTE YOUR WEEK4 EXERCISE CODE HERE */
+let updatedogLicensesSmallBounds = function() {dogMap.flyToBounds(dogLicensesBoundSmall)};
 
-let updateSchoolMarkers = (schoolsToShow) => {
-  schoolLayer.clearLayers();
-  highlightLayer.clearLayers();
-  schoolsToShow.forEach((school) => {
-    let [lat, lng] = school['GPS Location'].split(',');
-    let stationName = school['Publication Name'];
-    let marker = L.marker([lat, lng]).bindTooltip(stationName);
-    marker.addEventListener('click', () => {
-      highlightLayer.clearLayers();
-      showSchoolInfo(school, marker);
-      let highlightMarker = L.marker([lat, lng], { icon: redIcon });
-      highlightLayer.addLayer(highlightMarker);
+/* show the dog runs */
+
+const dogRunsStyle = {
+  "color": "#ff7800",
+  "weight": 5,
+  "opacity": 0.65
+};
+
+let updateDogRunsMarkers = (dogRuns) => {
+  /* celar layer*/
+  layerGroup.clearLayers();
+  /* fly to bounds*/
+	let dogRunBound = L.geoJSON(dogRuns,{style: dogRunsStyle});
+	dogMap.flyToBounds(dogRunBound.getBounds());
+  /* Loop each dog run to plot it*/
+  dogRuns.forEach(dogRun => {
+    let dogRunpolygon = L.geoJSON([dogRun],{style: dogRunsStyle})
+    .bindTooltip(dogRun.properties.name)
+    .addTo(layerGroup);
+    let dogRunName = dogRun.properties.name
+  /* Add event listener*/
+    dogRunpolygon.addEventListener('click', () => {
+      dogRunpolygon.bindPopup(
+        `<h6>${dogRunName}</h6>`
+        ).openPopup();
     });
-    schoolLayer.addLayer(marker);
   });
 };
 
-let updateSchoolList = (schoolsToShow) => {
-  schoolList.innerHTML = '';
-  HighLightSchool.innerHTML = '';
-  let schoolList2 = schoolsToShow.map(school => school['Publication Name']);
-  schoolList2.forEach(school => {
-    let el3 = htmlToElement(`<li>${school}</li>`);
-    schoolList.appendChild(el3);
+/* show the pet store */
+
+const petStoreStyle = {
+  color: "#EF6190",
+  fillColor: "#EF6190",
+  fillOpacity: 0.5,
+};
+
+let updatePetStoresMarkers = (petStores) => {
+  /* celar layer*/
+  layerGroup.clearLayers();
+	/* fly to bounds*/
+  dogMap.flyToBounds(petStoresBound);
+  /* Loop each dog store to plot it*/
+  petStores.forEach(PetStore => {
+    let circleMarker = L.circle([PetStore.lat, PetStore.lon],40, petStoreStyle)
+    .bindTooltip(PetStore.tags.name)
+    .addTo(layerGroup);
+    let petStoreName = PetStore.tags.name
+  /* Add event listener*/
+    circleMarker.addEventListener('click', () => {
+      circleMarker.bindPopup(
+        `<h6>${petStoreName}</h6>`
+        ).openPopup();
+    });
   });
 };
 
-let initializeZipCodeChoices = () => {
-  let zipCodeList = schools.map(school => school['Zip Code'].split('-')[0]);
-  let uniqueZipCodeList = [...new Set(zipCodeList)];
-  uniqueZipCodeList.forEach(uniqueZipCode => {
-    let el = htmlToElement(`<option>${uniqueZipCode}</option>`);
-    zipCodeSelect.appendChild(el);
-  });
+/* initial function*/
+calcCurrentSlideIndex()
+document.addEventListener('scroll', calcCurrentSlideIndex);
+
+/* Animation Title*/
+let canvas = document.querySelector('.the-canvas');
+let context = canvas.getContext('2d');
+let ratio = window.devicePixelRatio || 1;
+
+let totalLineHeight = 1000;
+let totalLines = 3;
+let totalDiff = totalLineHeight / totalLines;
+let fontHeight = 60 * ratio - 50; // Small centering
+
+let smallestWidth = 280; // width of smallest line;
+let offsetX = 10;
+let offsetY = 1;
+var iterations;
+var verticalAlign, line1Diff, line2Diff, line3Diff, line4Diff, iterations, iteration, animationFrame;
+
+let startRGB = [100, 255, 255];
+let endRGB   = [220, 165, 163];
+let fullColorSet = [];
+
+init();
+
+function init() {
+  
+  // Cancel any already running animations
+  cancelAnimationFrame(animationFrame);
+  // Set the canvas width and height
+  canvas.width = window.innerWidth * ratio;
+  canvas.height = window.innerHeight * ratio;
+  // Set the canvas font properties
+  context.font = '350px Varela Round';
+  context.textAlign = 'center';
+  context.strokeStyle = "#F3BE4E";
+  context.lineWidth = "5";
+  context.textBaseline = "middle"; 
+  // Centering of the text
+  verticalAlign = (window.innerHeight / 2  * ratio) - totalLineHeight / 2;
+  line2Diff = totalLineHeight + fontHeight - totalDiff * 1;
+  line3Diff = totalLineHeight + fontHeight - totalDiff * 2;
+  line4Diff = totalLineHeight + fontHeight - totalDiff * 3;
+  // How many iterations will we go through?
+  iterations = Math.floor(((window.innerWidth * ratio / 2) - (smallestWidth * ratio / 2)) / offsetX + 5);
+  prepareColorSets(iterations);
+  iteration = 0;
+  animationFrame = requestAnimationFrame(draw);
+}
+
+// Draw loop
+function draw() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  for( let i = iterations - 1; i > 0; i-- ) {
+    context.fillStyle = 'rgb(' + fullColorSet[i][0] + ',' + fullColorSet[i][1] + ',' + fullColorSet[i][2] + ')';
+    let x = window.innerWidth / 2 * ratio - i * offsetX;
+    let y = verticalAlign + i * offsetY + (Math.sin(i + iteration) * 2);
+    drawText( x, y );
+  } 
+  iteration += 0.1; 
+  animationFrame = requestAnimationFrame(draw);
+}
+
+// Draw the single lines of text.
+function drawText(x, y) {
+  context.fillText("Doggy", x, y + line4Diff);
+  context.strokeText("Doggy", x, y + line4Diff);
+  context.fillText("in", x, y + line3Diff);
+  context.strokeText("in", x, y + line3Diff);
+  context.fillText("New York City", x, y + line2Diff);
+  context.strokeText("New York City", x, y + line2Diff);
+}
+
+// We do this so we don't have to calculate these EVERY loop.
+function prepareColorSets(iterations) {
+  fullColorSet = [];
+  for( var i = 0; i < iterations; i++ ) {
+    fullColorSet.push(colourGradientor(1 - i / iterations, startRGB, endRGB));
+  }
+}
+
+// THNX - http://stackoverflow.com/questions/14482226/how-can-i-get-the-color-halfway-between-two-colors
+function colourGradientor(p, rgb_beginning, rgb_end){
+
+  var w = p * 2 - 1;
+  var w1 = (w + 1) / 2.0;
+  var w2 = 1 - w1;
+  var rgb = [parseInt(rgb_beginning[0] * w1 + rgb_end[0] * w2),
+             parseInt(rgb_beginning[1] * w1 + rgb_end[1] * w2),
+             parseInt(rgb_beginning[2] * w1 + rgb_end[2] * w2)];
+  return rgb;
 };
 
-let filteredSchools = () => {
-  let selectedGrade = gradeLevelSelect.options[gradeLevelSelect.selectedIndex].text;
-  let selectedZipCode = zipCodeSelect.options[zipCodeSelect.selectedIndex].text;
-  if (selectedGrade === 'All' && selectedZipCode === 'All') {
-    return schools;
-  } if (selectedGrade === 'All' && selectedZipCode !== 'All') {
-    return schools.filter(school => school['Zip Code'].split('-')[0] === selectedZipCode);
-  } if (selectedGrade !== 'All' && selectedZipCode === 'All') {
-    return schools.filter(school => school[`${selectedGrade}`] === '1');
-  } return schools.filter(school => school[`${selectedGrade}`] === '1' && school['Zip Code'].split('-')[0] === selectedZipCode);
-};
-
-/*
-
-No need to edit anything below this line ... though feel free.
-
-*/
-
-// The handleSelectChange function is an event listener that will be used to
-// update the displayed schools when one of the select filters is changed.
-let handleSelectChange = () => {
-  const schoolsToShow = filteredSchools() || [];
-  updateSchoolMarkers(schoolsToShow);
-  updateSchoolList(schoolsToShow);
-};
-
-gradeLevelSelect.addEventListener('change', handleSelectChange);
-zipCodeSelect.addEventListener('change', handleSelectChange);
-
-// The code below will be run when this script first loads. Think of it as the
-// initialization step for the web page.
-initializeZipCodeChoices();
-updateSchoolMarkers(schools);
-updateSchoolList(schools);
+window.onresize = init;
