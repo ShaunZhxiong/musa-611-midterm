454931828c2c58d55a0f96aa4d6ff44e2b6c293e
Shaun.xiongzheng <shaun_zheng@163.com>
2022-03-27 21:49:54 -0400

    style

--- dogImgParticular.js
+++ dogImgParticular.js
@@ -1,25 +1,23 @@
-/* Animation Title*/
-let canvas = document.querySelector('.the-canvas');
-let context = canvas.getContext('2d');
-let ratio = window.devicePixelRatio || 1;
+/* Animation Title */
+const canvas = document.querySelector('.the-canvas');
+const context = canvas.getContext('2d');
+const ratio = window.devicePixelRatio || 1;
 
-let totalLineHeight = 1200;
-let totalLines = 3;
-let totalDiff = totalLineHeight / totalLines;
-let fontHeight = 60 * ratio - 50; // Small centering
+const totalLineHeight = 1200;
+const totalLines = 3;
+const totalDiff = totalLineHeight / totalLines;
+const fontHeight = 60 * ratio - 50; // Small centering
 
-let smallestWidth = 280; // width of smallest line;
-let offsetX = 15;
-let offsetY = 1;
+const offsetX = 15;
+const offsetY = 1;
 var iterations;
-var verticalAlign, line1Diff, line2Diff, line3Diff, line4Diff, iterations, iteration, animationFrame;
+var verticalAlign, line2Diff, line3Diff, line4Diff, iterations, iteration, animationFrame;
 
-let startRGB = [255, 186, 73];
-let endRGB = [194, 205, 215];
+const startRGB = [255, 186, 73];
+const endRGB = [194, 205, 215];
 let fullColorSet = [];
 
 function init() {
-
   // Cancel any already running animations
   cancelAnimationFrame(animationFrame);
   // Set the canvas width and height
@@ -28,11 +26,11 @@ function init() {
   // Set the canvas font properties
   context.font = '350px Varela Round';
   context.textAlign = 'center';
-  context.strokeStyle = "#EF6190";
-  context.lineWidth = "5";
-  context.textBaseline = "middle";
+  context.strokeStyle = '#EF6190';
+  context.lineWidth = '5';
+  context.textBaseline = 'middle';
   // Centering of the text
-  verticalAlign = (window.innerHeight / 2 * ratio) - totalLineHeight / 2;
+  verticalAlign = ((window.innerHeight / 2) * ratio) - totalLineHeight / 2;
   line2Diff = totalLineHeight + fontHeight - totalDiff * 1;
   line3Diff = totalLineHeight + fontHeight - totalDiff * 2;
   line4Diff = totalLineHeight + fontHeight - totalDiff * 3;
@@ -48,8 +46,8 @@ function draw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   for (let i = iterations - 1; i > 0; i--) {
     context.fillStyle = 'rgb(' + fullColorSet[i][0] + ',' + fullColorSet[i][1] + ',' + fullColorSet[i][2] + ')';
-    let x = window.innerWidth / 2 * ratio - i * offsetX;
-    let y = verticalAlign + i * offsetY + (Math.sin(i + iteration) * 2);
+    const x = (window.innerWidth / 2) * ratio - (i * offsetX);
+    const y = verticalAlign + i * offsetY + (Math.sin(i + iteration) * 2);
     drawText(x, y);
   }
   iteration += 1;
@@ -58,12 +56,12 @@ function draw() {
 
 // Draw the single lines of text.
 function drawText(x, y) {
-  context.fillText("DDDogs", x, y + line4Diff);
-  context.strokeText("DDDogs", x, y + line4Diff);
-  context.fillText("in", x, y + line3Diff);
-  context.strokeText("in", x, y + line3Diff);
-  context.fillText("New York City", x, y + line2Diff);
-  context.strokeText("New York City", x, y + line2Diff);
+  context.fillText('DDDogs', x, y + line4Diff);
+  context.strokeText('DDDogs', x, y + line4Diff);
+  context.fillText('in', x, y + line3Diff);
+  context.strokeText('in', x, y + line3Diff);
+  context.fillText('New York City', x, y + line2Diff);
+  context.strokeText('New York City', x, y + line2Diff);
 }
 
 // We do this so we don't have to calculate these EVERY loop.
@@ -76,15 +74,13 @@ function prepareColorSets(iterations) {
 
 // THNX - http://stackoverflow.com/questions/14482226/how-can-i-get-the-color-halfway-between-two-colors
 function colourGradientor(p, rgb_beginning, rgb_end) {
-
-  var w = p * 2 - 1;
-  var w1 = (w + 1) / 2.0;
-  var w2 = 1 - w1;
-  var rgb = [parseInt(rgb_beginning[0] * w1 + rgb_end[0] * w2),
+  const w = p * 2 - 1;
+  const w1 = (w + 1) / 2.0;
+  const w2 = 1 - w1;
+  const rgb = [parseInt(rgb_beginning[0] * w1 + rgb_end[0] * w2),
   parseInt(rgb_beginning[1] * w1 + rgb_end[1] * w2),
   parseInt(rgb_beginning[2] * w1 + rgb_end[2] * w2)];
   return rgb;
-};
-
+}
 
 window.onresize = init;
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dogLicenseGroup = L.layerGroup().addTo(dogMap);
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   subdomains: 'abcd',
-  maxZoom: 20
+  maxZoom: 20,
 }).addTo(dogMap);
 
 const dogLicensesBoundSmall = [
@@ -13,7 +13,7 @@ const dogLicensesBoundSmall = [
   [40.70250, -73.93524],
   [40.79509, -73.93524],
   [40.79509, -74.02656],
-  [40.70250, -74.02656]
+  [40.70250, -74.02656],
 ];
 
 const petStoresBound = [
@@ -21,8 +21,224 @@ const petStoresBound = [
   [40.660847697284815, -73.87859344482422],
   [40.823682398765996, -73.87859344482422],
   [40.823682398765996, -74.0478515625],
-  [40.660847697284815, -74.0478515625]
+  [40.660847697284815, -74.0478515625],
 ];
+
+/* show the dog breeds */
+
+const dogLicensesStyle = {
+  color: '#FFD000',
+  fillColor: '#FFD000',
+  fillOpacity: 0.5,
+};
+
+const dogLicensesBound = L.geoJSON(dogLicenses);
+
+const updatedogLicensesMarkers = (dogLicenses) => {
+  /* celar layer  */
+  layerGroup.clearLayers();
+  dogLicenseGroup.clearLayers();
+  /* fly to bounds  */
+  dogMap.flyToBounds(dogLicensesBoundSmall);
+  /* Loop each dog store to plot it */
+  dogLicenses.forEach((dogLicense) => {
+    const circleMarker = L.circle([dogLicense.geometry.coordinates[1], dogLicense.geometry.coordinates[0]], 40, dogLicensesStyle)
+      .bindTooltip(dogLicense.properties.GEOID.toString())
+      .addTo(layerGroup);
+    const dogLicensesGEOID = dogLicense.properties.GEOID.toString();
+    /* Add event listener */
+    circleMarker.addEventListener('click', () => {
+      zipCodeBoundary(dogLicense.properties.GEOID);
+      circleMarker.bindPopup(
+        `<h6>${dogLicensesGEOID}</h6>
+        <ul>
+        <li>Total Numer: ${dogLicense.properties.count.toString()} </li>
+        <li>Age: ${dogLicense.properties.Age.toString()} </li>
+        <li>Female %: ${dogLicense.properties.Femalperc.toString()} </li>
+        <li>Male %: ${dogLicense.properties.Maleperc.toString()} </li></ul>`,
+        ).openPopup();
+      });
+  });
+};
+
+const zipCodeBoundary = (zipcode) => {
+  dogLicenseGroup.clearLayers();
+  const dataUrl = `data/zipcodePoly/${zipcode}.geojson`;
+  $.getJSON(dataUrl, function (data) {
+    const dogLicensesPoly = L.geoJson(data, dogLicensesStyle).addTo(dogLicenseGroup)
+      .addEventListener('click', () => {
+        dogLicenseGroup.clearLayers();
+        if (zipcode !== 98363) {
+          updatedogLicensesMarkers(dogLicenses);
+        } else updatedogLicensesFarest(dogLicenses);
+      });
+    dogMap.flyToBounds(dogLicensesPoly.getBounds());
+  });
+};
+
+$('#form1').submit( function(e) {
+  e.preventDefault();
+  const selectZipcodeElement = document.getElementById('zipcodeTypeIn');
+  selectZipcode = selectZipcodeElement.value;
+  if (selectZipcode === 'all') {
+    updatedogLicensesMarkers(dogLicenses);
+  } else zipCodedogLicenses(selectZipcode);
+});
+
+let zipCodedogLicenses = (selectZipcode) => {
+  /* celar layer  */
+  layerGroup.clearLayers();
+  /* Loop each dog store to plot it */
+  dogLicenses.forEach( (dogLicense) => {
+    if (dogLicense.properties.GEOID.toString() === selectZipcode) {
+      const circleMarker = L.circle([dogLicense.geometry.coordinates[1], dogLicense.geometry.coordinates[0]], 40, dogLicensesStyle)
+        .bindTooltip(dogLicense.properties.GEOID.toString())
+        .addTo(layerGroup);
+      const dogLicensesGEOID = dogLicense.properties.GEOID.toString();
+      zipCodeBoundary(dogLicense.properties.GEOID);
+      circleMarker.bindPopup(
+        `<h6>${dogLicensesGEOID}</h6>
+        <ul>
+        <li>Total Numer: ${dogLicense.properties.count.toString()} </li>
+        <li>Age: ${dogLicense.properties.Age.toString()} </li>
+        <li>Female %: ${dogLicense.properties.Femalperc.toString()} </li>
+        <li>Male %: ${dogLicense.properties.Maleperc.toString()} </li></ul>`,
+      ).openPopup();
+    }
+  });
+};
+
+let updatedogLicensesFarest = (dogLicenses) => {
+  /* FIND THE farrest ONE */
+  dogLicenses.forEach((dogLicense) => {
+    if (dogLicense.properties.GEOID === 98363) {
+      const farestLicense = dogLicense;
+      /* celar layer  */
+      layerGroup.clearLayers();
+      /* fly to bounds  */
+      const dogLicensesBound = L.latLng(farestLicense.geometry.coordinates[1], farestLicense.geometry.coordinates[0]);
+      dogMap.flyToBounds(dogLicensesBound.toBounds(100000));
+      /* PLOT IT  */
+      zipCodeBoundary(dogLicense.properties.GEOID);
+      const circleMarker = L.circle([farestLicense.geometry.coordinates[1], farestLicense.geometry.coordinates[0]], 40, dogLicensesStyle)
+        .bindTooltip(farestLicense.properties.GEOID.toString())
+        .addTo(layerGroup);
+      const dogLicensesGEOID = farestLicense.properties.GEOID.toString();
+      circleMarker.bindPopup(
+        `<h6>${dogLicensesGEOID}</h6>
+        <ul>
+        <li>Total Numer: ${farestLicense.properties.count.toString()} </li>
+        <li>Age: ${farestLicense.properties.Age.toString()} </li>
+        <li>Female %: ${farestLicense.properties.Femalperc.toString()} </li>
+        <li>Male %: ${farestLicense.properties.Maleperc.toString()} </li></ul>`,
+      ).openPopup();
+    }
+  });
+};
+
+/* show the dog runs */
+
+const dogRunsStyle = {
+  color: '#ff7800',
+  weight: 5,
+  opacity: 0.65,
+};
+
+const dogRunBound = L.geoJSON(dogRuns, { style: dogRunsStyle });
+
+const updateDogRunsMarkers = (dogRuns) => {
+  /* celar layer  */
+  layerGroup.clearLayers();
+  /* fly to bounds  */
+  dogMap.flyToBounds(dogRunBound.getBounds());
+  /* Loop each dog run to plot it */
+  dogRuns.forEach((dogRun) => {
+    const dogRunpolygon = L.geoJSON([dogRun], { style: dogRunsStyle })
+      .bindTooltip(dogRun.properties.name)
+      .addTo(layerGroup);
+    /* Add event listener */
+    dogRunpolygon.addEventListener('click', () => {
+      dogMap.flyToBounds(dogRunpolygon.getBounds());
+      dogRunpolygon.bindPopup(
+        `<h6>${dogRun.properties.name}</h6>
+        <ul>
+          <li>Zipcode:${dogRun.properties.zipcode} </li>
+          <li>Area: ${Math.round(dogRun.properties.area)} sqm</li>
+          <li><a href='https://www.google.com/search?q=${dogRun.properties.name}' target='_blank'>More Info</a></li></ul>`,
+      ).openPopup();
+      dogRunpolygon.addEventListener('click', () => {
+        updateDogRunsMarkers(dogRuns);
+      });
+    });
+  });
+};
+
+const updateDogRunsBiggest = (dogRuns) => {
+  /* FIND THE BIGGEST ONE */
+  dogRuns.forEach((dogRun) => {
+    if (dogRun.properties.name === 'Rockaway Freeway Dog Park') {
+      const biggestDogRun = dogRun;
+      /* celar layer  */
+      layerGroup.clearLayers();
+      /* fly to bounds  */
+      const dogRunBound = L.geoJSON([biggestDogRun], { style: dogRunsStyle });
+      dogMap.flyToBounds(dogRunBound.getBounds().pad(0.75));
+      /* PLOT IT  */
+      const dogRunpolygon = L.geoJSON([biggestDogRun], { style: dogRunsStyle })
+        .bindTooltip(biggestDogRun.properties.name)
+        .addTo(layerGroup);
+      /* Add event listener */
+      dogRunpolygon.addEventListener('click', () => {
+        dogRunpolygon.bindPopup(
+          `<h6>${dogRun.properties.name}</h6>
+          <ul>
+            <li>Zipcode:${dogRun.properties.zipcode} </li>
+            <li>Area: </li>
+            <li><a href='https://www.google.com/search?q=${dogRun.properties.name}'>More Info</a></li>
+          </ul>`,
+        ).openPopup();
+      });
+    }
+  });
+};
+
+/* show the pet store */
+
+const petStoreStyle = {
+  color: '#EF6190',
+  fillColor: '#EF6190',
+  fillOpacity: 0.5,
+};
+
+const updatePetStoresMarkers = (petStores) => {
+  /* celar layer  */
+  layerGroup.clearLayers();
+  /* fly to bounds  */
+  dogMap.flyToBounds(petStoresBound);
+  /* Loop each dog store to plot it */
+  petStores.forEach((PetStore) => {
+    const circleMarker = L.circle([PetStore.lat, PetStore.lon], 40, petStoreStyle)
+      .bindTooltip(PetStore.tags.name)
+      .addTo(layerGroup);
+    /* Add event listener */
+    circleMarker.addEventListener('click', () => {
+      circleMarker.bindPopup(
+        JSON.stringify(PetStore.tags)
+          .replace('{', '')
+          .replace('}', '')
+          .replaceAll('"', '')
+          .replaceAll('addr:', '')
+          .replaceAll(':', ':\ ')
+          .replaceAll(',', '<br>')
+          .toUpperCase(),
+        // PetStore.tags.forEach( tag => {
+        //   console.log(tag);
+        // })
+        // `<h6>${PetStore.tags[0]}</h6>`
+      ).openPopup();
+    });
+  });
+};
 
 /* current slide index */
 let currentSlideIndex = -1;
@@ -46,7 +262,7 @@ function calcCurrentSlideIndex() {
       layerGroup.clearLayers();
       init();
     } else {
-      cancelAnimationFrame(animationFrame)
+      cancelAnimationFrame(animationFrame);
       if (currentSlideIndex === 1) {
         updateDogRunsMarkers(dogRuns);
       } else if (currentSlideIndex === 2) {
@@ -57,236 +273,12 @@ function calcCurrentSlideIndex() {
         updatedogLicensesFarest(dogLicenses);
       } else if (currentSlideIndex === 5) {
         updatePetStoresMarkers(petStores);
-      };
+      }
     }
   }
 }
-/* show the dog breeds */
 
-const dogLicensesStyle = {
-  color: "#FFD000",
-  fillColor: "#FFD000",
-  fillOpacity: 0.5,
-};
-
-let dogLicensesBound = L.geoJSON(dogLicenses);
-
-let zipCodeBoundary = (zipcode) => {
-  dogLicenseGroup.clearLayers();
-  const dataUrl = `data/zipcodePoly/${zipcode}.geojson`;
-  $.getJSON(dataUrl, function(data) {
-    
-    let dogLicensesPoly = L.geoJson(data,dogLicensesStyle).addTo(dogLicenseGroup)
-      .addEventListener('click', () => {
-        dogLicenseGroup.clearLayers();
-        if (zipcode !== 98363) {
-          updatedogLicensesMarkers(dogLicenses);
-        } else updatedogLicensesFarest(dogLicenses);
-      });
-      dogMap.flyToBounds(dogLicensesPoly.getBounds());
-  }
-)};
-
-$("#form1").submit(function(e) {
-  e.preventDefault();
-  let selectZipcodeElement = document.getElementById("zipcodeTypeIn");
-  selectZipcode = selectZipcodeElement.value;
-  if (selectZipcode === "all") {
-    updatedogLicensesMarkers(dogLicenses);
-  } else zipCodedogLicenses(selectZipcode);
-});
-
-let updatedogLicensesMarkers = (dogLicenses) => {
-  /* celar layer*/
-  layerGroup.clearLayers();
-  dogLicenseGroup.clearLayers();
-  /* fly to bounds*/
-  dogMap.flyToBounds(dogLicensesBoundSmall);
-  /* Loop each dog store to plot it*/
-  dogLicenses.forEach(dogLicense => {
-    let circleMarker = L.circle([dogLicense.geometry.coordinates[1], dogLicense.geometry.coordinates[0]], 40, dogLicensesStyle)
-      .bindTooltip(dogLicense.properties.GEOID.toString())
-      .addTo(layerGroup);
-    let dogLicensesGEOID = dogLicense.properties.GEOID.toString();
-    /* Add event listener*/
-    circleMarker.addEventListener('click', () => {
-      zipCodeBoundary(dogLicense.properties.GEOID);
-      circleMarker.bindPopup(
-        `<h6>${dogLicensesGEOID}</h6>
-        <ul>
-        <li>Total Numer: ${dogLicense.properties.count.toString()} </li>
-        <li>Age: ${dogLicense.properties.Age.toString()} </li>
-        <li>Female %: ${dogLicense.properties.Femalperc.toString()} </li>
-        <li>Male %: ${dogLicense.properties.Maleperc.toString()} </li>
-      </ul>`
-      ).openPopup();
-    });
-  });
-};
-
-let zipCodedogLicenses = (selectZipcode) => {
-  /* celar layer*/
-  layerGroup.clearLayers();
-  /* Loop each dog store to plot it*/
-  dogLicenses.forEach(dogLicense => {
-    if (dogLicense.properties.GEOID.toString() === selectZipcode) {
-      let circleMarker = L.circle([dogLicense.geometry.coordinates[1], dogLicense.geometry.coordinates[0]], 40, dogLicensesStyle)
-        .bindTooltip(dogLicense.properties.GEOID.toString())
-        .addTo(layerGroup);
-      let dogLicensesGEOID = dogLicense.properties.GEOID.toString();
-      zipCodeBoundary(dogLicense.properties.GEOID);
-      circleMarker.bindPopup(
-        `<h6>${dogLicensesGEOID}</h6>
-        <ul>
-        <li>Total Numer: ${dogLicense.properties.count.toString()} </li>
-        <li>Age: ${dogLicense.properties.Age.toString()} </li>
-        <li>Female %: ${dogLicense.properties.Femalperc.toString()} </li>
-        <li>Male %: ${dogLicense.properties.Maleperc.toString()} </li>
-      </ul>`
-      ).openPopup();
-    }
-  });
-};
-
-let updatedogLicensesFarest = (dogLicenses) => {
-  /* FIND THE farrest ONE*/
-  dogLicenses.forEach(dogLicense => {
-    if (dogLicense.properties.GEOID === 98363) {
-      let farestLicense = dogLicense;
-      /* celar layer*/
-      layerGroup.clearLayers();
-      /* fly to bounds*/
-      let dogLicensesBound = L.latLng(farestLicense.geometry.coordinates[1], farestLicense.geometry.coordinates[0]);
-      dogMap.flyToBounds(dogLicensesBound.toBounds(100000));
-      /* PLOT IT*/
-      zipCodeBoundary(dogLicense.properties.GEOID);
-      let circleMarker = L.circle([farestLicense.geometry.coordinates[1], farestLicense.geometry.coordinates[0]], 40, dogLicensesStyle)
-        .bindTooltip(farestLicense.properties.GEOID.toString())
-        .addTo(layerGroup);
-      let dogLicensesGEOID = farestLicense.properties.GEOID.toString();
-      circleMarker.bindPopup(
-        `<h6>${dogLicensesGEOID}</h6>
-        <ul>
-        <li>Total Numer: ${farestLicense.properties.count.toString()} </li>
-        <li>Age: ${farestLicense.properties.Age.toString()} </li>
-        <li>Female %: ${farestLicense.properties.Femalperc.toString()} </li>
-        <li>Male %: ${farestLicense.properties.Maleperc.toString()} </li>
-      </ul>`
-      ).openPopup();
-    };
-  });
-};
-
-/* show the dog runs */
-
-const dogRunsStyle = {
-  "color": "#ff7800",
-  "weight": 5,
-  "opacity": 0.65
-};
-
-let dogRunBound = L.geoJSON(dogRuns, { style: dogRunsStyle });
-
-let updateDogRunsMarkers = (dogRuns) => {
-  /* celar layer*/
-  layerGroup.clearLayers();
-  /* fly to bounds*/
-  dogMap.flyToBounds(dogRunBound.getBounds());
-  /* Loop each dog run to plot it*/
-  dogRuns.forEach(dogRun => {
-    let dogRunpolygon = L.geoJSON([dogRun], { style: dogRunsStyle })
-      .bindTooltip(dogRun.properties.name)
-      .addTo(layerGroup);
-    /* Add event listener*/
-    dogRunpolygon.addEventListener('click', () => {
-      dogMap.flyToBounds(dogRunpolygon.getBounds());
-      dogRunpolygon.bindPopup(
-        `<h6>${dogRun.properties.name}</h6>
-        <ul>
-          <li>Zipcode:${dogRun.properties.zipcode} </li>
-          <li>Area: ${Math.round(dogRun.properties.area)} sqm</li>
-          <li><a href="https://www.google.com/search?q=${dogRun.properties.name}" target="_blank">More Info</a></li>
-        </ul>`
-      ).openPopup();
-      dogRunpolygon.addEventListener('click', () => {
-        updateDogRunsMarkers(dogRuns);
-      });
-    });
-  });
-};
-
-let updateDogRunsBiggest = (dogRuns) => {
-  /* FIND THE BIGGEST ONE*/
-  dogRuns.forEach(dogRun => {
-    if (dogRun.properties.name === "Rockaway Freeway Dog Park") {
-      let biggestDogRun = dogRun;
-      /* celar layer*/
-      layerGroup.clearLayers();
-      /* fly to bounds*/
-      let dogRunBound = L.geoJSON([biggestDogRun], { style: dogRunsStyle });
-      dogMap.flyToBounds(dogRunBound.getBounds().pad(0.75));
-      /* PLOT IT*/
-      let dogRunpolygon = L.geoJSON([biggestDogRun], { style: dogRunsStyle })
-        .bindTooltip(biggestDogRun.properties.name)
-        .addTo(layerGroup);
-      let dogRunName = biggestDogRun.properties.name
-      /* Add event listener*/
-      dogRunpolygon.addEventListener('click', () => {
-        dogRunpolygon.bindPopup(
-          `<h6>${dogRun.properties.name}</h6>
-          <ul>
-            <li>Zipcode:${dogRun.properties.zipcode} </li>
-            <li>Area: </li>
-            <li><a href="https://www.google.com/search?q=${dogRun.properties.name}">More Info</a></li>
-          </ul>`
-        ).openPopup();
-      });
-    };
-  });
-};
-
-/* show the pet store */
-
-const petStoreStyle = {
-  color: "#EF6190",
-  fillColor: "#EF6190",
-  fillOpacity: 0.5,
-};
-
-let updatePetStoresMarkers = (petStores) => {
-  /* celar layer*/
-  layerGroup.clearLayers();
-  /* fly to bounds*/
-  dogMap.flyToBounds(petStoresBound);
-  /* Loop each dog store to plot it*/
-  petStores.forEach(PetStore => {
-    let circleMarker = L.circle([PetStore.lat, PetStore.lon], 40, petStoreStyle)
-      .bindTooltip(PetStore.tags.name)
-      .addTo(layerGroup);
-    let petStoreName = PetStore.tags.name
-    /* Add event listener*/
-    circleMarker.addEventListener('click', () => {
-      circleMarker.bindPopup(
-        JSON.stringify(PetStore.tags)
-          .replace('{', '')
-          .replace('}', '')
-          .replaceAll('"', '')
-          .replaceAll('addr:', '')
-          .replaceAll(':', ':\ ')
-          .replaceAll(',', '<br>')
-          .toUpperCase()
-        // PetStore.tags.forEach( tag => {
-        //   console.log(tag);
-        // })
-        // `<h6>${PetStore.tags[0]}</h6>`
-      ).openPopup();
-    });
-  });
-};
-
-
-
-/* initial function*/
+/* initial function */
 init();
 calcCurrentSlideIndex();
 document.addEventListener('scroll', calcCurrentSlideIndex);
